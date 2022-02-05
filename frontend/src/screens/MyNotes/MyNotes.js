db95b38f19e93772d1827a8dd76e48a052c1379e
MasahiroKaga <masahiro41802891@gmail.com>
2022-02-04 19:36:45 -0800

    #7 Add Mynotes comp and css 7:00 - 14:00

--- frontend/src/screens/MyNotes/MyNotes.js
+++ frontend/src/screens/MyNotes/MyNotes.js
@@ -1,10 +1,17 @@
 import React from "react";
+import { Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import MainScreen from "../../components/MainScreen";
 
 const MyNotes = () => {
   return (
-    <div>
-      <h1>test</h1>
-    </div>
+    <MainScreen title="Welcome Back Masahiro Kaga...">
+      <Link to="createnote">
+        <Button style={{marginLeft:10,marginBottom:6}} size="lg">
+          Create New Note
+        </Button>
+      </Link>
+    </MainScreen>
   );
 };
 
