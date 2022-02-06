4caee3d7bc4a24bd143fce1299ac35b6a3e5d8b6
MasahiroKaga <masahiro41802891@gmail.com>
2022-02-05 18:03:57 -0800

    #9 -23:30 Error handling [memo]

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,8 @@ const notes = require("./data/notes");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const connectDB = require("./config/db");
-const userRoutes = require("./routes/userRoutes")
+const userRoutes = require("./routes/userRoutes");
+const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
 
 const app = express();
 app.use(cors());
@@ -20,6 +21,9 @@ app.get("/api/notes", (req, res) => {
 });
 
 app.use("/api/users", userRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT= process.env.PORT || 4000;
 
