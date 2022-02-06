9367b94d0bf53268427daf8b83a5a6c4c58c7171
MasahiroKaga <masahiro41802891@gmail.com>
2022-02-05 20:34:38 -0800

    #9 -45:00 Add login system on LoginScreen component and try post [MEMO]

--- frontend/src/screens/LoginScreen/LoginScreen.js
+++ frontend/src/screens/LoginScreen/LoginScreen.js
@@ -1,7 +1,74 @@
-import React from 'react';
+import React, { useState } from "react";
+import { Button, Col, Form, Row } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import MainScreen from "../../components/MainScreen";
+import "./LoginScreen.css";
+import axios from "axios";
 
 const LoginScreen = () => {
-  return <div>login</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState("");
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    try {
+      const config = {
+        headers: {
+          "Content-type": "application/json",
+        },
+      };
+      setLoading(true);
+      const { data } = await axios.post(
+        "http://localhost:4000/api/users/login",
+        { email, password },
+        config
+      );
+      console.log(data);
+      localStorage.setItem('userInfo',JSON.stringify(data));
+      setLoading(false);
+    } catch (error) {
+        setError(error.response.data.message)
+    }
+  };
+
+  return (
+    <MainScreen title="LOGIN">
+      <div className="loginContainer">
+        <Form onSubmit={submitHandler}>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control
+              type="email"
+              value={email}
+              placeholder="Enter email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </Form.Group>
+
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              value={password}
+              placeholder="Password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </Form.Group>
+
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+        <Row className="py-3">
+          <Col>
+            New Customer ? <Link to="/register">Register Here</Link>
+          </Col>
+        </Row>
+      </div>
+    </MainScreen>
+  );
 };
 
 export default LoginScreen;
