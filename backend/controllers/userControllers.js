c0b2c499f8abe6dc0ee9ffcafef7b46bc7fff2d1
MasahiroKaga <masahiro41802891@gmail.com>
2022-02-05 18:32:29 -0800

    #9 -30:00 Login system with authentication [memo]

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -21,17 +21,34 @@ const registerUser = asyncHandler(async (req, res) => {
   if (user) {
     res.status(201).json({
       _id: user._id,
-      name:user.name,
-      email:user.email,
-      isAdmin:user.isAdmin,
-      pic:user.pic
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      pic: user.pic,
     });
-  }else{
-      res.status(400);
-      throw new Error("Error Occured!");
+  } else {
+    res.status(400);
+    throw new Error("Error Occured!");
   }
-
-  
 });
 
-module.exports = { registerUser };
+const authUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      pic: user.pic
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid Email or Password");
+  }
+});
+
+module.exports = { registerUser, authUser };
