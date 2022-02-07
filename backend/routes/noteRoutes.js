b9964b93bdef9b4d4c29b34105bfd78da50c4f8b
MasahiroKaga <masahiro41802891@gmail.com>
2022-02-06 23:49:19 -0800

    #12 18:00 Creat note API

--- backend/routes/noteRoutes.js
+++ backend/routes/noteRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { getNotes } = require("../controllers/noteControllers");
+const { getNotes, createNote } = require("../controllers/noteControllers");
 const { protect } = require("../middlewares/authMiddleware");
 
 const router = express.Router();
 router.route('/').get(protect,getNotes);
+router.route("/create").post(protect,createNote);
 
 module.exports = router;
