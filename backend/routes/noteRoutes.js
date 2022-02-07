c5cf3268a9012c6e65d58166e65967acfd53f2eb
MasahiroKaga <masahiro41802891@gmail.com>
2022-02-06 22:42:15 -0800

    #12 12:40 Add authentication middleware

--- backend/routes/noteRoutes.js
+++ backend/routes/noteRoutes.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const { getNotes } = require("../controllers/noteControllers");
+const { protect } = require("../middlewares/authMiddleware");
 
 const router = express.Router();
-router.route('/').get(getNotes);
+router.route('/').get(protect,getNotes);
 
 module.exports = router;
