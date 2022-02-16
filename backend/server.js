f7230eb7e1c85abb7a4bb889eb71c62f2348174d
MasahiroKaga <masahiro41802891@gmail.com>
2022-02-16 13:45:47 -0800

    make it better

--- backend/server.js
+++ backend/server.js
@@ -6,16 +6,13 @@ const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
 const noteRoutes = require("./routes/noteRoutes");
 const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
+const path = require("path");
 
 const app = express();
 app.use(cors());
 dotenv.config();
 connectDB();
 app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send("API is running");
-});
 
 // app.get("/api/notes", (req, res) => {
 //   res.json(notes);
@@ -24,10 +21,27 @@ app.get("/", (req, res) => {
 app.use("/api/users", userRoutes);
 app.use("/api/notes", noteRoutes);
 
+//--------------------deployment--------------------
+
+__dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+} 
+
+//--------------------deployment--------------------
 
 app.use(notFound);
 app.use(errorHandler);
 
-const PORT= process.env.PORT || 4000;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, console.log(`Server started on ${PORT}`));
